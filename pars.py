88ac93be4599eb83094659030b4e6fa5a80d72b2
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-01-25 20:03:58 +0700

    добавил вывод даты

--- pars.py
+++ pars.py
@@ -3,14 +3,19 @@ import lxml.html
 
 #В этом файле представлены два по сути одинаковых кода, которые лишь обращаются к разным страницам сайта
 
-"""Вариант парсинга со страницы Python Events"""
+"""Вариант парсинга со страницы Python Events
+    Дополнительно выводим место проведения события с помощью f-строки
+    А так же дату проведения"""
+
 html = requests.get('https://www.python.org/events/python-events/').content
 
 tree = lxml.html.document_fromstring(html)
-events = tree.xpath('/html/body/div/div[3]/div/section/div/div/ul/li/h3')
+events = tree.xpath('/html/body/div/div[3]/div/section/div/div/ul/li')
 for li in events:
-    a = li.find('a')
-    print(a.text)
+    event = (li.find('h3/a')).text
+    place = (li.find('p/span')).text
+    date = (li.find('p/time')).text
+    print(f'"{event}" in {place}, date: {date}')
 
 print('\n')
 
